f33ecfe5d083c438d65814872f5e20809af7306e
zakifaisal05official <154603762+zakifaisal05official@users.noreply.github.com>
2026-01-23 18:38:28 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,39 @@
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+const { handleMessage } = require('./handler');
+const fs = require('fs');
+
+const client = new Client({
+    authStrategy: new LocalAuth({ dataPath: './sessions' }),
+    puppeteer: {
+        headless: true,
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--no-zygote',
+            '--single-process'
+        ],
+    }
+});
+
+client.on('qr', (qr) => {
+    console.log('📢 SCAN QR CODE SEKARANG:');
+    qrcode.generate(qr, { small: true });
+});
+
+client.on('ready', () => {
+    console.log('✅ BOT BERHASIL ONLINE DI RAILWAY!');
+});
+
+client.on('message_create', async (msg) => {
+    if (msg.fromMe) return;
+    try {
+        await handleMessage(client, msg);
+    } catch (e) {
+        console.error(e);
+    }
+});
+
+client.initialize();
+
