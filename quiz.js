efe3f5f2fddf686eb90632bed345750f99289a76
zakifaisal05official <154603762+zakifaisal05official@users.noreply.github.com>
2026-01-26 16:10:06 +0700

    Create quiz.js

---
+++ quiz.js
@@ -0,0 +1,23 @@
+// quiz.js
+const QUIZ_BANK = [
+    // Senin
+    { question: "Gimana pelajaran IPA tadi? Banyak rumus gak?", options: ["Pusing banget", "Biasa aja", "Seru percobaannya", "Ngantuk"] },
+    { question: "Bahasa Inggris tadi aman? Udah paham materinya?", options: ["Paham dong", "Masih bingung", "Skip dulu", "Lumayan"] },
+    // Selasa
+    { question: "Tadi latihan buat Pensi gimana? Udah ada kemajuan?", options: ["Lancar jaya", "Masih kaku", "Seru banget", "Capek tapi asik"] },
+    { question: "Gimana Matematika hari ini? Otak masih aman?", options: ["Aman terkendali", "Berasap dikit", "Tolong menyerah", "Biasa aja"] },
+    // Rabu
+    { question: "Bahasa Sunda tadi lancar? Bisa ngomongnya?", options: ["Lancar pisan", "Belum bisa", "Dikit-dikit", "Bingung"] },
+    { question: "IPS hari ini bahas apa? Seru gak materinya?", options: ["Seru", "Bosenin", "Banyak nyatet", "Lupa"] },
+    // Kamis
+    { question: "Persiapan Pensi tadi bahas bagian apa?", options: ["Konsep/Ide", "Latihan Gerak", "Cari properti", "Diskusi bareng"] },
+    { question: "Pelajaran Pancasila tadi bahas apa nih?", options: ["Diskusi", "Presentasi", "Nyatet", "Dengerin aja"] },
+    // Jumat
+    { question: "Gimana Jumsih & Yasinan tadi pagi?", options: ["Adem banget", "Capek bersih-bersih", "Seru", "Telat"] },
+    { question: "Bahasa Cirebon tadi gimana? Bisa bahasanya?", options: ["Bisa dikit", "Susah banget", "Lancar", "Cuma nyatet"] },
+    // Umum
+    { question: "Paling suka pelajaran apa hari ini?", options: ["Pelajaran Jam pertama", "Pelajaran Terakhir", "Istirahat", "Semuanya seru"] },
+    { question: "Mood balik sekolah hari ini gimana?", options: ["Happy", "Laper", "Ngantuk", "Butuh healing"] }
+];
+
+module.exports = { QUIZ_BANK };
